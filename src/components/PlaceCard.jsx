01e19b084e20be7ef87e458e44ccdbd739afde7a
JENNY-257 <j.iribagiza2@gmail.com>
2023-08-31 23:43:04 +0200

    adding cart component

--- src/components/PlaceCard.jsx
+++ src/components/PlaceCard.jsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PlaceCard = ({ placeName, imageUrl }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const handleAddToCart = () => {
+    setIsCartOpen(true);
+  };
+
+  const handleCloseCart = () => {
+    setIsCartOpen(false);
+  };
+
   return (
     <div className="bg-white shadow-lg rounded-md overflow-hidden">
       <img src={imageUrl} alt={placeName} className="w-full h-40 object-cover" />
       <div className="p-9">
         <h3 className="text-lg font-semibold mb-2">{placeName}</h3>
         <p className="text-gray-500">Explore the beauty of {placeName}</p>
+        <button
+          onClick={handleAddToCart}
+          className="bg-gray-600 text-white py-2 px-4 rounded hover:bg-gray-700 focus:outline-none focus:ring focus:border-gray-400"
+        >
+          Add to Cart
+        </button>
       </div>
+
+      {isCartOpen && (
+        <div className="fixed inset-0 flex justify-center items-center bg-gray-700 bg-opacity-50">
+          <div className="bg-white border rounded p-4 w-[400px]">
+            {/* Close button */}
+            <button
+              onClick={handleCloseCart}
+              className="absolute top-2 right-2 text-gray-600 hover:text-gray-800 focus:outline-none"
+            >
+              X
+            </button>
+            {/* Popup content */}
+            <h2 className="text-lg font-semibold mb-2 ">Add to Cart</h2>
+            <form className="flex flex-col w-[80px]">
+              <label className="mb-2 w-[700px]">Number of Tickets</label>
+              <input type="number" className="border border-gray-300 rounded-md py-2 px-3 mb-3 w-[300px]" />
+              <label className="mb-2 w-[700px]">Select Company</label>
+              <select className="border border-gray-300 rounded-md py-2 px-3 mb-3 w-[300px]">
+                <option value="company1">Company 1</option>
+                <option value="company2">Company 2</option>
+              </select>
+              <label className="mb-8 w-[700px]">Select Location with Price</label>
+              <select className="border border-gray-300 rounded-md py-2 px-3 mb-3 w-[300px]">
+                <option value="location1">$100 - Location 1</option>
+                <option value="location2">$150 - Location 2</option>
+              </select>
+              <button
+                onClick={handleCloseCart}
+                className="bg-gray-500 text-white py-2 px-0 rounded hover:bg-gray-900 focus:outline-none focus:ring focus:border-blue-300"
+              >
+                Checkout
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/TicketCart.jsx
+++ src/components/TicketCart.jsx
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TicketCart = () => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [ticketDetails, setTicketDetails] = useState({
+    selectedCompany: '',
+    selectedLocation: '',
+    locationPrice: 0,
+    numberOfTickets: 1,
+  });
+
+  const companies = [
+    { name: 'Company A', locations: [{ name: 'Location 1', price: 50 }, { name: 'Location 2', price: 70 }] },
+    { name: 'Company B', locations: [{ name: 'Location 3', price: 60 }, { name: 'Location 4', price: 80 }] },
+    // Add more companies and locations as needed
+  ];
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setTicketDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: value,
+    }));
+  };
+
+  const calculateTotalPrice = () => {
+    // Calculate the total price based on the location price and number of tickets
+    return ticketDetails.locationPrice * ticketDetails.numberOfTickets;
+  };
+
+  const handleProceedToCheckout = () => {
+    // You can implement the logic to proceed to checkout here
+  };
+
   return (
     <div>
-      <h2>Ticket Cart</h2>
-      <ul>
-        {/* Display selected tickets */}
-        <li>Ticket 1</li>
-        <li>Ticket 2</li>
-        {/* Add more tickets */}
-      </ul>
-      <button>Proceed to Checkout</button>
+      <button
+        onClick={() => setIsCartOpen(true)}
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+      >
+        Open Cart
+      </button>
+
+      {isCartOpen && (
+        <div className="fixed inset-0 flex justify-center items-center bg-gray-700 bg-opacity-50">
+          <div className="bg-white border rounded p-4 w-[400px]">
+            <h2 className="text-lg font-semibold mb-3">Ticket Cart</h2>
+            <div className="mb-3">
+              {/* Rest of the form components */}
+              {/* ... */}
+            </div>
+            <div className="mb-3">
+              {/* Rest of the form components */}
+              {/* ... */}
+            </div>
+            <button
+              onClick={() => setIsCartOpen(false)}
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+            >
+              Close Cart
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
